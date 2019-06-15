e197a713a18b5c221d2e739565bd0ddc2fe7a9b0
p0tr3c <potr3c@protonmail.com>
2019-06-15 19:39:09 +0100

    Fix terrible bash

--- tests/aws_run.sh
+++ tests/aws_run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 function execute_test(){
-    /bin/bash "${1}.sh"
+    /bin/bash "${1}.sh" > /dev/null
     if [ "$?" -ne "0" ]; then
-        echo "FAIL: $1"
-        cat "${1}.log"
         return -1
     fi
-    echo "PASS: $1"
     return 0
 }
 
 declare -a aws_tests=(
-    "test_distro_sources",
+    "test_distro_sources"
     "test_distro_sources_signatures")
 
 for aws_test in "${aws_tests[@]}"
 do
-    res=execute_test "$aws_test"
-    if [ $res -ne 0 ]; then
+    execute_test $aws_test
+    if [ "$?" -ne 0 ]; then
+        echo "FAIL: $aws_test"
+        cat "${aws_test}.log"
         exit 1
     fi
+    echo "PASS: $aws_test"
 done
