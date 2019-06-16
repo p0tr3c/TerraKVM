afe5371f37cacd4a020f240d853927eb7107ec91
p0tr3c <potr3c@protonmail.com>
2019-06-16 20:08:11 +0100

    Update env

--- tests/terraform/google_cloud/test_support/deployment_env.sh
+++ tests/terraform/google_cloud/test_support/deployment_env.sh
@@ -3,6 +3,9 @@ export TF_ENV_tf_state_bucket=`aws ssm get-parameters --name /terrakvm/tf_state_
 export TF_ENV_aws_region=`aws ssm get-parameters --name /terrakvm/aws_region --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 export TF_ENV_state_key=`aws ssm get-parameters --name /terrakvm/tf_state_key --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 export GOOGLE_CLOUD_KEYFILE_JSON=`aws ssm get-parameters --name /terrakvm/gcloud_access_file --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
+export GOOGLE_PROJECT=`aws ssm get-parameters --name /terrakvm/gcloud_project --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
+export GOOGLE_REGION=`aws ssm get-parameters --name /terrakvm/gcloud_region --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
+export TF_ENV_gcloud_zone=`aws ssm get-parameters --name /terrakvm/gcloud_compute_zone --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 
 aws s3 cp "s3://${TF_ENV_tf_state_bucket}/${GOOGLE_CLOUD_KEYFILE_JSON}" ./
 if [ "$?" - ne 0 ];then
