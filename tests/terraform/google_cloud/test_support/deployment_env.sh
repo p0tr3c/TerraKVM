ec043ef549b10ecbb83d9d51e233fa8184228d0f
p0tr3c <potr3c@protonmail.com>
2019-06-16 21:05:09 +0100

    Update parameter name

--- tests/terraform/google_cloud/test_support/deployment_env.sh
+++ tests/terraform/google_cloud/test_support/deployment_env.sh
@@ -5,7 +5,7 @@ export TF_ENV_state_key=`aws ssm get-parameters --name /terrakvm/tf_state_key --
 export GCLOUD_KEYFILE_JSON=`aws ssm get-parameters --name /terrakvm/gcloud_access_file --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 export GOOGLE_PROJECT=`aws ssm get-parameters --name /terrakvm/gcloud_project --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 export GOOGLE_REGION=`aws ssm get-parameters --name /terrakvm/gcloud_region --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
-export TF_VAR_gcloud_zone=`aws ssm get-parameters --name /terrakvm/gcloud_compute_zone --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
+export TF_VAR_gcloud_zone=`aws ssm get-parameters --name /terrakvm/gcloud_zone --region "$SSM_REGION" | jq -r '.Parameters[0].Value'`
 
 aws s3 cp "s3://${TF_ENV_tf_state_bucket}/${GCLOUD_KEYFILE_JSON}" ./
 if [ "$?" -ne "0" ];then
